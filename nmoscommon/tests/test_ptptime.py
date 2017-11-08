8ccf592b24b3f28b91585af2a4979cf2e2ed4d23
Simon Rankine <simon.rankine@bbc.co.uk>
2017-11-08 12:08:05 +0000

    Add in missing tests from ipppython

---
+++ nmoscommon/tests/test_ptptime.py
@@ -0,0 +1,32 @@
+import unittest
+import time
+
+from nmoscommon import ptptime
+
+
+class PTPTest(unittest.TestCase):
+
+    def test_ptp_detail(self):
+        rv = ptptime.ptp_detail()
+        self.assertEqual(2, len(rv))
+        self.assertTrue(isinstance(rv[0], int))
+        self.assertTrue(isinstance(rv[1], int))
+
+    def test_fallback(self):
+        """
+        Fallback mechanism is close to PTP time.
+        When the fallback mechanism for getting a current time is triggered,
+        the returned time should be the same as the PTP time that *would*
+        have been returned if the fallback wasn't triggered.
+        """
+        # In order to get this to work, we force ptp_detail to raise an exception
+        # when accessing clock_gettime later.
+        # We also get the current ptp/sysclock offset and ensure that is applied.
+        def throw(*args):
+            raise Exception()
+
+        ptp_sec, _ = ptptime.ptp_detail()
+        sec_offset = ptp_sec - int(time.time())
+        ptptime.clock_gettime = throw
+        fallback_sec, _ = ptptime.ptp_detail()
+        self.assertEqual(ptp_sec, fallback_sec - sec_offset)

f21955c89c2694acaea3bd53507a001d6917391e
James P. Weaver <james.barrett@bbc.co.uk>
2017-12-04 18:33:12 +0000

    ptptime: 100% test coverage

--- tests/test_ptptime.py
+++ tests/test_ptptime.py
@@ -1,31 +1,54 @@
 import unittest
 import time
 
-from nmoscommon import ptptime
+import nmoscommon
+from nmoscommon.ptptime import *
+import mock
 
+orig_import = __import__
+
+def import_mock(magic_name, allow):
+    def __inner(name, *args):
+        if name == magic_name:
+            if allow:
+                return mock.MagicMock()
+            else:
+                raise ImportError
+        else:
+            return orig_import(name, *args)
+    return __inner
 
 class PTPTest(unittest.TestCase):
 
+    def test_import_noerror(self):
+        with mock.patch('__builtin__.__import__', side_effect=import_mock("ipppython.ptptime",True)):
+            reload(nmoscommon.ptptime)
+            from nmoscommon.ptptime import IPP_PYTHON
+            self.assertTrue(IPP_PYTHON)
+    
+    def test_import_error(self):
+        with mock.patch('__builtin__.__import__', side_effect=import_mock("ipppython.ptptime",False)):
+            reload(nmoscommon.ptptime)
+            from nmoscommon.ptptime import IPP_PYTHON
+            self.assertFalse(IPP_PYTHON)
+
     def test_ptp_detail(self):
-        rv = ptptime.ptp_detail()
-        self.assertEqual(2, len(rv))
-        self.assertTrue(isinstance(rv[0], int))
-        self.assertTrue(isinstance(rv[1], int))
+        """Testing the fallback when IPP_PYTHON doesn't load"""
+        with mock.patch('__builtin__.__import__', side_effect=import_mock("ipppython.ptptime",False)):
+            reload(nmoscommon.ptptime)
+            from nmoscommon.ptptime import ptp_detail
+            rv = ptp_detail()
+            self.assertEqual(2, len(rv))
+            self.assertTrue(isinstance(rv[0], int))
+            self.assertTrue(isinstance(rv[1], int))
 
     def test_fallback(self):
-        """
-        Fallback mechanism is close to PTP time.
-        When the fallback mechanism for getting a current time is triggered,
-        the returned time should be the same as the PTP time that *would*
-        have been returned if the fallback wasn't triggered.
-        """
-        # In order to get this to work, we force ptp_detail to raise an exception
-        # when accessing clock_gettime later.
-        # The TAI/UTC time offset should already be included
-        def throw(*args):
-            raise Exception()
-
-        ptp_sec, _ = ptptime.ptp_detail()
-        ptptime.clock_gettime = throw
-        fallback_sec, _ = ptptime.ptp_detail()
-        self.assertEqual(ptp_sec, fallback_sec)
+        with mock.patch('nmoscommon.ptptime.Timestamp.get_time') as get_time:
+            with mock.patch('__builtin__.__import__', side_effect=import_mock("ipppython.ptptime",False)):
+                tval = 23*1e9 + 17
+                get_time.return_value.to_nanosec.return_value = tval
+                reload(nmoscommon.ptptime)
+                from nmoscommon.ptptime import ptp_time
+                ts = ptp_time()
+                get_time.assert_called_once_with()
+                self.assertEqual(ts, tval/1e9)

b1ca24d79c2fc7c49f291cb212d183424905d774
Simon Rankine <simon.rankine@bbc.co.uk>
2018-03-19 15:25:22 +0000

    Bump version number to get around broken package

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ deps_required = [
 
 
 setup(name="nmoscommon",
-      version="0.1.1",
+      version="0.1.3",
       description="nmos python utilities",
       url='www.nmos.tv',
       author='Peter Brightwell',

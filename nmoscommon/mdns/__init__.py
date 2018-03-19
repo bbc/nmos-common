15caf7ac380f984b7256a15f774190a03ba1d369
Simon Rankine <simon.rankine@bbc.co.uk>
2018-03-19 15:18:56 +0000

    Revert "Linting"
    This reverts commit c98921d99f39fdab4de5762058feb0a3d880de0f.

--- nmoscommon/mdns/__init__.py
+++ nmoscommon/mdns/__init__.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 try:
-    import avahi  # noqa: F401
-except ImportError:  # pragma: no cover
+    import avahi
+except: # pragma: no cover
     from .bonjour import MDNSEngine
-else:  # pragma: no cover
+else: # pragma: no cover
     from .avahidbus import MDNSEngine
 
-__all__ = ["MDNSEngine"]
+__all__ = [ "MDNSEngine" ]

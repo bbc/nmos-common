a21034b2d9884be06a1ac57f9b24a8d547a35103
Simon Rankine <simon.rankine@bbc.co.uk>
2018-12-07 15:31:23 +0000

    Deep-copy "value" when for add_resource and update_resource in facade (#61)
    * 'value' should be deep copied into the class in case the client modifies it later
    
    * Make facade file pass linting

--- nmoscommon/facade.py
+++ nmoscommon/facade.py
@@ -15,7 +15,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import os
-import traceback
 from .ipc import Proxy
 import gevent
 from threading import Lock
@@ -29,29 +28,31 @@ FAC_UNAUTHORISED = 3
 FAC_UNSUPPORTED = 4
 FAC_OTHERERROR = 5
 
+
 class Facade(object):
     """This class serves as a proxy for the Facade running on the same machine if it exists. If no facade exists
     on this machine then it will do nothing, but calls will still function without throwing any exceptions."""
-    def __init__(self, srv_type, address="ipc:///tmp/ips-nodefacade",logger=None):
-        self.logger          = Logger("facade_proxy", logger)
-        self.ipc             = None
-        self.srv_registered  = False # Flag whether service is registered
-        self.reregister      = False # Flag whether resources are correctly registered
-        self.address         = address
-        self.srv_type        = srv_type.lower()
-        self.srv_type_urn    = "urn:x-ipstudio:service:" + self.srv_type
-        self.pid             = os.getpid()
-        self.resources       = {}
-        self.controls        = {}
-        self.href            = None
-        self.proxy_path      = None
-        self.lock            = Lock() # Protect access to IPC socket
+    def __init__(self, srv_type, address="ipc:///tmp/ips-nodefacade", logger=None):
+
+        self.logger = Logger("facade_proxy", logger)
+        self.ipc = None
+        self.srv_registered = False  # Flag whether service is registered
+        self.reregister = False  # Flag whether resources are correctly registered
+        self.address = address
+        self.srv_type = srv_type.lower()
+        self.srv_type_urn = "urn:x-ipstudio:service:" + self.srv_type
+        self.pid = os.getpid()
+        self.resources = {}
+        self.controls = {}
+        self.href = None
+        self.proxy_path = None
+        self.lock = Lock()  # Protect access to IPC socket
 
     def setup_ipc(self):
         with self.lock:
             try:
                 self.ipc = Proxy(self.address)
-            except:
+            except Exception:
                 self.ipc = None
 
     def register_service(self, href, proxy_path):
@@ -173,12 +174,14 @@ class Facade(object):
             self.reregister = True
 
     def addResource(self, type, key, value):
+        value = deepcopy(value)
         if type not in self.resources:
             self.resources[type] = {}
         self.resources[type][key] = value
         self._call_ipc_method("res_register", type, key, value)
 
     def updateResource(self, type, key, value):
+        value = deepcopy(value)
         if type not in self.resources:
             self.resources[type] = {}
         self.resources[type][key] = value
@@ -219,10 +222,10 @@ class Facade(object):
         self._call_ipc_method("clk_unregister", clk_name)
 
     def debug_message(self, code):
-        msg =  {FAC_SUCCESS:"Success!",
-                FAC_EXISTS:"Service already exists",
-                FAC_UNREGISTERED:"Service isn't yet registered",
-                FAC_UNAUTHORISED:"Unauthorised",
-                FAC_UNSUPPORTED:"Unsupported",
-                FAC_OTHERERROR:"Other error"} [code]
+        msg = {FAC_SUCCESS: "Success!",
+               FAC_EXISTS: "Service already exists",
+               FAC_UNREGISTERED: "Service isn't yet registered",
+               FAC_UNAUTHORISED: "Unauthorised",
+               FAC_UNSUPPORTED: "Unsupported",
+               FAC_OTHERERROR: "Other error"}[code]
         return msg

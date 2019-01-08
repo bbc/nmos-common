e57fd02707a2f669c9b7429a1ce348970a8e2529
Andrew Bonney <andrew.bonney@bbc.co.uk>
2019-01-08 17:28:04 +0000

    dnsutils: fix import bug in dns library

--- nmoscommon/mdns/dnsUtils.py
+++ nmoscommon/mdns/dnsUtils.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import dns
+import dns.resolver  # Importing dns alone is not enough for NXDOMAIN to work
 from nmoscommon.mdns.mdnsExceptions import DNSRecordNotFound
 
 

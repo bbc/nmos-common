596cd9469162d491c0342e99f57b302b05fbdb4a
Andrew Bonney <andrew.bonney@bbc.co.uk>
2019-01-28 13:48:22 +0000

    mdnslistener: fix bug in type vs. srv_type naming

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ deps_required = []
 
 
 setup(name="nmoscommon",
-      version="0.14.0",
+      version="0.14.1",
       description="Common components for the BBC's NMOS implementations",
       url='https://github.com/bbc/nmos-common',
       author='Peter Brightwell',

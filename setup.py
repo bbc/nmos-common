324e457150d571b40180e24ad31ca8bd59c476f5
Sam Mesterton-Gibbons <sam.mesterton-gibbons@bbc.co.uk>
2019-07-05 10:59:23 +0100

    Version bump: 0.19.9
    Allow use of newer Werkzeug by trying both import paths.
    Fixes #92 and improves on change in v0.17.2.

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ deps_required = []
 
 setup(
     name="nmoscommon",
-    version="0.19.8",
+    version="0.19.9",
     description="Common components for the BBC's NMOS implementations",
     url='https://github.com/bbc/nmos-common',
     author='Peter Brightwell',

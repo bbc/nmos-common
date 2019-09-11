0b92947b5b8a8d00bc17240274618dc8147d4c42
Danny Meloy <danny.meloy@bbc.co.uk>
2019-09-11 11:26:04 +0100

    patch version bump

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ deps_required = []
 
 setup(
     name="nmoscommon",
-    version="0.19.13",
+    version="0.19.14",
     description="Common components for the BBC's NMOS implementations",
     url='https://github.com/bbc/nmos-common',
     author='Peter Brightwell',
